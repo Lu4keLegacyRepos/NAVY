3fb94b446289d8a55c4fb387ba16d02430e4493c
Lukas Drabek <dra0060@vsb.cz>
2020-02-26 22:44:37 +0100

    cv3 init

---
+++ NNLayer/Layer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace NeuralNetwork
+{
+    public class Layer
+    {
+        public int NodesCount { get; private set; }
+        private Perceptron.Perceptron[] nodes;
+
+        public Layer(int nodesCount, (int numOfInputs, Func<double, int> activationFunc) percepsData)
+        {
+            NodesCount = nodesCount;
+            nodes = new Perceptron.Perceptron[NodesCount];
+            for (int i = 0; i < NodesCount; i++)
+            {
+                nodes[i] = new Perceptron.Perceptron(percepsData.numOfInputs, percepsData.activationFunc);
+            }
+        }
+    }
+}
